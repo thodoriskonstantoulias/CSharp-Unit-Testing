00759badd708f58811c6cfc758aa61fef92b9dab
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-09 21:09:40 +0200

    Testing strings

---
+++ TestNinja/TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldReturnStringWithStrongElement()
+        {
+            var htmlFormat = new HTMLFormatter();
+
+            var result = htmlFormat.FormatAsBold("abc");
+
+            //Specific assertion
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+
+            //More general assertion
+            Assert.That(result, Does.StartWith("<strong>"));
+
+            //Better 
+            Assert.That(result, Does.Contain("abc"));
+        }
+    }
+}
---
+++ TestNinja/TestNinja/Fundamentals/HTMLFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNinja.Fundamentals
+{
+    public class HTMLFormatter
+    {
+        public string FormatAsBold(string content)
+        {
+            return $"<strong>{content}</strong>";
+        }
+    }
+}
