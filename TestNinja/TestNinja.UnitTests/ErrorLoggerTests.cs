f2d5331b597c641aad7a1a98d80a4f1c83d45f1a
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-09 22:31:55 +0200

    Testing methods that throws exceptions

---
+++ TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ShouldThrowException(string error)
+        {
+            var logger = new ErrorLogger();
+
+            //logger.Log(error);
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+        }
+    }
+}
---
+++ TestNinja/TestNinja/Fundamentals/ErrorLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNinja.Fundamentals
+{
+    public class ErrorLogger
+    {
+        public string LastError { get; set; }
+        public event EventHandler<Guid> ErrorLogged;
+
+        public void Log(string error)
+        {
+            if (String.IsNullOrWhiteSpace(error))
+            {
+                throw new ArgumentNullException();
+            }
+            LastError = error;
+            ErrorLogged?.Invoke(this, Guid.NewGuid());
+        }
+    }
+}
