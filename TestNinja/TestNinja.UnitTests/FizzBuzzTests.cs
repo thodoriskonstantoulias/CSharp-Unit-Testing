38f551a90a86b4e4154f4921036c2c07052b8394
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-10 21:36:50 +0200

    Exercise 1 : FizzBuzz class Unit Test

---
+++ TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_NumberDividesByThreeAndFive_ReturnsFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+
+            Assert.That(result, Is.EqualTo("Fizz Buzz"));
+        }
+        [Test]
+        public void GetOutput_NumberDividesByThree_ReturnsFizz()
+        {
+            var result = FizzBuzz.GetOutput(6);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+        [Test]
+        public void GetOutput_NumberDividesByFive_ReturnsBuzz()
+        {
+            var result = FizzBuzz.GetOutput(10);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+        [Test]
+        public void GetOutput_NumberDoesNotDividesByThreeOrFive_ReturnsNumber()
+        {
+            var result = FizzBuzz.GetOutput(11);
+
+            Assert.That(result, Is.EqualTo("11"));
+        }
+    }
+}
---
+++ TestNinja/TestNinja/Fundamentals/FizzBuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNinja.Fundamentals
+{
+    public class FizzBuzz
+    {
+        public static string GetOutput(int number)
+        {
+            if ((number % 3 == 0) && (number % 5 == 0))
+            {
+                return "Fizz Buzz";
+            }
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            return number.ToString();
+        }
+    }
+}
