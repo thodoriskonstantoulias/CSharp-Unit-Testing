235ed3b8a20c23def52593392de0fa1a9d786d41
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-12-11 21:27:28 +0200

    Dependency injection via method parameters

---
+++ TestNinja/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    class VideoServiceTests
+    {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnsErrorMessage()
+        {
+            var service = new VideoService();
+            var result = service.ReadVideoTitle(new FakeFileReader());
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
---
+++ TestNinja/TestNinja/Mocking/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNinja.Mocking
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var service = new VideoService();
+
+            //Dependency injection via method parameters
+            var title = service.ReadVideoTitle(new FileReader());
+
+        }
+    }
+}
